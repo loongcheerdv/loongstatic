f10b82af3be49b4676ff4ce4381bc6ea3796f4e2
liuhm252 <l2102362>
2020-09-18 16:44:00 +0800

    update

--- static/js/news_card.js
+++ static/js/news_card.js
@@ -1,45 +1,28 @@
 $(function() {
     $("#News").addClass('active');
-    $("#news_title_1").hover(function() {
-        $("#new_content_1").css('display', 'block');
-        $("#new_content_2").css('display', 'none');
-        $("#new_content_3").css('display', 'none');
-        $("#new_content_4").css('display', 'none');
-        $("#news_title_1").addClass("select");
-        $("#news_title_2").removeClass("select");
-        $("#news_title_3").removeClass("select");
-        $("#news_title_4").removeClass("select");
-        // get_page();
-    });
-    $("#news_title_2").hover(function() {
-        $("#new_content_1").css('display', 'none');
-        $("#new_content_2").css('display', 'block');
-        $("#new_content_3").css('display', 'none');
-        $("#new_content_4").css('display', 'none');
+    let select = window.location.search.match('news_type=(.*)&')[1];
+    if(select==="Company"){
         $("#news_title_1").removeClass("select");
         $("#news_title_2").addClass("select");
         $("#news_title_3").removeClass("select");
         $("#news_title_4").removeClass("select");
-    });
-    $("#news_title_3").hover(function() {
-        $("#new_content_1").css('display', 'none');
-        $("#new_content_2").css('display', 'none');
-        $("#new_content_3").css('display', 'block');
-        $("#new_content_4").css('display', 'none');
+    }
+    else if(select==="Industry"){
         $("#news_title_1").removeClass("select");
         $("#news_title_2").removeClass("select");
         $("#news_title_3").addClass("select");
         $("#news_title_4").removeClass("select");
-    });
-    $("#news_title_4").hover(function() {
-        $("#new_content_1").css('display', 'none');
-        $("#new_content_2").css('display', 'none');
-        $("#new_content_3").css('display', 'none');
-        $("#new_content_4").css('display', 'block');
+    }
+    else if(select==="Media"){
         $("#news_title_1").removeClass("select");
         $("#news_title_2").removeClass("select");
         $("#news_title_3").removeClass("select");
         $("#news_title_4").addClass("select");
-    });
-
+    }
+    else{
+        $("#news_title_1").addClass("select");
+        $("#news_title_2").removeClass("select");
+        $("#news_title_3").removeClass("select");
+        $("#news_title_4").removeClass("select");
+    }
 });
